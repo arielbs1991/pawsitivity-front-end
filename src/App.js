99f9a417b59d08896d05766d906af2d1435b3487
wolfdog27 <kylenicholasanderson@gmail.com>
2020-08-13 13:57:12 -0700

    set up App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from "./pages/Login"
+import Profile from './pages/Profile';
+import Swipe from './pages/Swipe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+    <Switch>
+      <Route path="/login">
+        <Login/>
+      </Route>
+      <Route path = "/swipe">
+        <Swipe/>
+      </Route>
+      <Route path = "/profile">
+        <Profile/>
+      </Route>
+      <Route exact path="/">
+        <Login/>
+      </Route>
+    </Switch>
+  </Router>
   );
 }
+
+// TODO: make navbar in components to put on each page
 
 export default App;
