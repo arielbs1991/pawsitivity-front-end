7fd55e8f58d266127f4b2d15c2f895734547aeb3
wolfdog27 <kylenicholasanderson@gmail.com>
2020-08-18 15:13:39 -0700

    logo plus petdetails style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,28 @@
 import React from 'react'
-import { PageHeader, Button } from 'antd';
+import { PageHeader, Button, Avatar } from 'antd';
 import {Link} from "react-router-dom"
+import { UserOutlined } from '@ant-design/icons';
+import { logDOM } from '@testing-library/react';
+import logo3Jpeg from '../Header/logo3.jpeg';
+
+
 
 export default function HeaderComp() {
     return (
         <PageHeader
             className="site-page-header-responsive"
-
-            title="Pawsitivity"
-            subTitle="This is a subtitle"
+            title={<img className="logo" alt="logo" src={logo3Jpeg} />}
+            
             extra={[
                 <Link class="headerLink" to="/login">Login</Link>,
                 <Link class="headerLink" to="/swipe">Swipe</Link>,
                 <Link class="headerLink" to="/profile">Profile</Link>,
                 <Link class="headerLink" to="/profile2">Profile2</Link>,
                 <Link class="headerLink" to="/updateprofile">UpdateProfile</Link>,
-                <Link class="headerLink" to="/matches">Matches</Link>
+                <Link class="headerLink" to="/matches">Matches</Link>,
+                <Link class="headerLink" to="/petdetails">Pet Details</Link>
             ]}
         />
 
     )
 }
-
-
