e1d77a3fe66e6c004f0990da723cdc8bd451cc54
Trish Ness <trishnss@gmail.com>
2020-08-18 10:02:37 -0700

    modals

--- src/components/animalCard/index.js
+++ src/components/animalCard/index.js
@@ -3,18 +3,19 @@ import { Button} from 'antd';
 import { CloseOutlined, CheckOutlined, InfoOutlined } from '@ant-design/icons';
 // import petAPI from "../../utils/petAPI"
 import matchAPI from "../../utils/matchAPI";
+import ModalComp from "../Modal"
 
 export default function AnimalCardComp(props) {
     return (
         
         <div className="animalCard">
             <img src={props.imageSrc}/>
-            <h1 align="center">{props.name}</h1>
+            <h1 align="center">{props.dog.name}</h1>
             <div className="pupCardOptions">
             <Button onClick={props.dislike} type="primary" danger shape="circle" icon={<CloseOutlined />}></Button>
             <Button onClick={props.like} type="primary" shape="circle" icon={<CheckOutlined />}></Button>
             </div>
-            <Button align="center" shape="circle" icon={<InfoOutlined />}></Button>
+            <ModalComp dog={props.dog}></ModalComp>
         </div>
     )
 }
