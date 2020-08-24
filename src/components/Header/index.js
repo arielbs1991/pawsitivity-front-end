3036f3697655bec9bebc0d09cf82750d5fc77521
wolfdog27 <kylenicholasanderson@gmail.com>
2020-08-24 11:02:34 -0700

    changed shelter header to Add Pet and Pet List

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { PageHeader } from 'antd';
+import { PageHeader} from 'antd';
 import { Link } from "react-router-dom"
 import logo3Jpeg from '../Header/logo3.jpeg';
 import LogoutComp from '../Logout'
-import "./style.css"
+
 
 export default function HeaderComp() {
     return (
@@ -12,10 +12,10 @@ export default function HeaderComp() {
                 title={<a href="/swipe"><img className="logo" Link="/swipe" alt="logo" src={logo3Jpeg} /></a>}
                 extra={[
                     // <Link key={1} className="headerLink" to="/login">Login</Link>,
-                    <Link justify='space-around' key={1} className="headerLink" to="/swipe">Swipe</Link>,
+                    <Link justify='space-around' key={2} className="headerLink" to="/swipe">Swipe</Link>,
                     // <Link key={3} className="headerLink" to="/profile2">Profile2</Link>,
-                    <Link justify='space-around' key={2} className="headerLink" to="/updateprofile">Profile</Link>,
-                    <Link justify='space-around' key={3} className="headerLink" to="/matches">Matches</Link>,
+                    <Link justify='space-around' key={4} className="headerLink" to="/updateprofile">Profile</Link>,
+                    <Link justify='space-around' key={5} className="headerLink" to="/matches">Matches</Link>,
                     // <Link key={6} className="headerLink" to="/petdetails">Pet Details</Link>,
                     <LogoutComp/>
                 ]}
