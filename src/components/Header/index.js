12aa0a8acd8cff8eb5824c3c34a97e9f42b8e38e
wolfdog27 <kylenicholasanderson@gmail.com>
2020-08-24 12:26:31 -0700

    all buttons on header same and hopefully no leftover border

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,8 @@ import { PageHeader} from 'antd';
 import { Link } from "react-router-dom"
 import logo3Jpeg from '../Header/logo3.jpeg';
 import LogoutComp from '../Logout'
+import "./style.css";
+
 
 
 export default function HeaderComp() {
@@ -11,9 +13,9 @@ export default function HeaderComp() {
                 className="site-page-header-responsive" //flex className to try and justify center align space around in css
                 title={<a href="/swipe"><img className="logo" Link="/swipe" alt="logo" src={logo3Jpeg} /></a>}
                 extra={[
-                    <Link justify='space-around' key={1} className="headerLink" to="/swipe">Swipe</Link>,
-                    <Link justify='space-around' key={2} className="headerLink" to="/updateprofile">Profile</Link>,
-                    <Link justify='space-around' key={3} className="headerLink" to="/matches">Matches</Link>,
+                    <button className="signUp3"><Link className="signUpBtn3" justify='space-around' key={1}  to="/swipe">Swipe</Link></button>,
+                    <button className="signUp3"><Link className="signUpBtn3" justify='space-around' key={2}  to="/updateprofile">Profile</Link></button>,
+                    <button className="signUp3"><Link className="signUpBtn3" justify='space-around' key={3}  to="/matches">Matches</Link></button>,
                     <LogoutComp/>
                 ]}
             />
