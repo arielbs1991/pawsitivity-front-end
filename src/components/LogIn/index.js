d1632321157d99c09c8a49f73d5dbf11b87c23d0
Cody Samuels <twowoodenspoons@gmail.com>
2020-08-24 10:46:57 -0700

    login feedback

--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -27,11 +27,14 @@ class LogInComp extends Component {
 
   handleFormSubmit = async (event) => {
     event.preventDefault();
-    if (!this.state.email) this.setState({ errorSpan: 'Please enter an email.' });
-    if (!this.state.password) this.setState({ errorSpan: 'Please enter a password.' });
 
-    await userAPI.login(this.state)
-    this.setState({ redirect: '/swipe' })
+    try {
+      await userAPI.login(this.state)
+      this.setState({ redirect: '/swipe' })
+    }
+    catch {
+      this.setState({ errorSpan: 'Login failed. Incorrect username or password.' })
+    }
   }
 
   render() {
@@ -62,9 +65,9 @@ class LogInComp extends Component {
           <br />
           <br />
           <div className="loginBtns">
-          <button className="signUp"><Link className="signUpBtn" to="/profile2"><strong>Sign Up</strong></Link></button>
+            <button className="signUp"><Link className="signUpBtn" to="/profile2"><strong>Sign Up</strong></Link></button>
             <br /><br />
-          <button className="loginButton" onClick={this.handleFormSubmit}><strong>Login</strong></button>
+            <button className="loginButton" onClick={this.handleFormSubmit}><strong>Login</strong></button>
           </div>
         </form>
       </div>
